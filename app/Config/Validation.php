d9da271dcb865dd3b15e679e4fb4e6ce6f5ba2af
chanlazaro <lazarochan03@gmail.com>
2021-04-27 16:29:32 +0800

    edited announcements

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -40,4 +40,49 @@ class Validation
 	//--------------------------------------------------------------------
 	// Rules
 	//--------------------------------------------------------------------
+
+		public $announcements = [
+		'title' => [
+			'label'  => 'title',
+			'rules'  => 'required|alpha_numeric_space',
+			'errors' => [
+				'required' => 'You must have {field} provided',
+				'alpha_numeric_space' => 'Title must include only alphanumeric characters or spaces.',
+			]
+		],
+
+		'description' => [
+			'label'  => 'description',
+			'rules'  => 'required|alpha_numeric_space',
+			'errors' => [
+				'required' => 'You must have {field} provided',
+				'alpha_numeric_space' => 'Title must include only alphanumeric characters or spaces.',
+			]
+		],
+
+		'start_date' => [
+			'label'  => 'date start',
+			'rules'  => 'required',
+			'errors' => [
+				'required' => 'You must have {field} provided'
+			]
+		],
+
+		'end_date' => [
+			'label'  => 'date end',
+			'rules'  => 'required',
+			'errors' => [
+				'required' => 'You must have {field} provided'
+			]
+		],
+
+		// images
+		'image' => [
+			'rules' => 'uploaded[image]|is_image[image]',
+			'errors' => [
+				'uploaded' => 'Please upload image.',
+				'is_image' => 'Uploaded file is not a valid image.',
+			]
+		],
+	];
 }
